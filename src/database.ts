cb6fff0a5a2097a13b2d01e0fb9db61588a492eb
Michael Corrigan <mcorrigan89@gmail.com>
2018-10-02 22:44:57 -0700

    docker

--- src/database.ts
+++ src/database.ts
@@ -4,10 +4,10 @@ import { Entities } from './entity';
 
 export const dbConnect = () => createConnection({
   type: 'postgres',
-  // host: 'localhost',
-  // port: 5432,
-  // database: 'openmic',
-  url: process.env.DATABASE_URL,
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'openmic',
+  // url: process.env.DATABASE_URL,
   entities: Object.keys(Entities).map(key => Entities[key]),
   synchronize: true,
   // dropSchema: true,
