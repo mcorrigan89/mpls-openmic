9a1e28bddde317604bc1cb0f20b3575de45a8878
Michael Corrigan <mcorrigan89@gmail.com>
2018-10-04 12:30:51 -0700

    standard entity

--- src/entity/showcase.ts
+++ src/entity/showcase.ts
@@ -1,8 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Timeslot } from './timeslot';
+import { Template } from './template';
 
 @Entity()
-export class Showcase {
+export class Showcase extends Template {
 
   @PrimaryGeneratedColumn('uuid')
   public id: string;
