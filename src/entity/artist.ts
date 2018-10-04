162f7c07bc9ac744bd791badf28fb68502984830
Michael Corrigan <mcorrigan89@gmail.com>
2018-10-04 12:33:34 -0700

    forgot to save

--- src/entity/artist.ts
+++ src/entity/artist.ts
@@ -1,8 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn,  OneToMany } from 'typeorm';
 import { Timeslot } from './timeslot';
+import { Template } from './template';
 
 @Entity()
-export class Artist {
+export class Artist extends Template {
 
   @PrimaryGeneratedColumn('uuid')
   public id: string;
